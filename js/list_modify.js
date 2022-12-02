74cd8af62f4e8c37833969b15df73cb684767008
diane-at-Okto <diane.kang@page-effect.com>
2022-12-02 17:00:52 +0100

    hauptKartesite filter fuktioniert

--- js/list_modify.js
+++ js/list_modify.js
@@ -30,18 +30,12 @@ document.getElementById('abschaltung_uhrzeit').addEventListener('change', functi
     console.log(uhr_group_text);
 
     if (uhr_group_text !="abschaltung_all"){
-        let elements = document.getElementsByClassName("unternehmenseintrag-filter");
-        for (i = 0; i < elements.length; i++) {
-            let uhr_value = elements[i].getAttribute("value");
-            console.log(filter_uhr_value);
-            if( parseFloat(uhr_value) < parseFloat(filter_uhr_value)){
-                elements[i].style.display = "block";
-                console.log(elements[i].getAttribute("value"));
-            }
-        }
         
-        // uhr_group = window[uhr_group_text];
-        // for (i = 0; i < uhr_group.length; i++)  uhr_group[i].style.display = "block";
+        let elements = document.getElementsByClassName(uhr_group_text);
+        
+        for (i = 0; i < elements.length; i++) {
+                elements[i].style.display = "block";
+             }
     }
     else{
         for (i = 0; i < all_unternehmen.length; i++) all_unternehmen[i].style.display = 'block';
